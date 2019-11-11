78db6754d985b0c9ec6667c397ebed3ca8369dd6
RandyN91 <randy.naraine64@gmail.com>
2019-11-11 10:10:12 -0500

    created function for HTML parser

--- main.py
+++ main.py
@@ -3,29 +3,33 @@ import xlrd
 import datetime
 import time
 
-file = open('report.html')
+htmlfile = open('report.html')
 tablelist = []
 
 excelfile = xlrd.open_workbook("Weekly CM Terminations.xlsx")
 excelSheet = excelfile.sheet_by_index(0)
 
-soup = bs4.BeautifulSoup(file,features="lxml")
-tables = soup.findChildren('table')
-my_table = tables[1]
-rows = my_table.findChildren(['th', 'tr'])
+def parseHTML(input):
+ soup = bs4.BeautifulSoup(htmlfile,features="lxml")
+ tables = soup.findChildren('table')
+ my_table = tables[1]
+ rows = my_table.findChildren(['th', 'tr'])
 
-for row in rows:
+ for row in rows:
         cells = row.findChildren('td')
         for cell in cells:
             value = str(cell.string).replace(u'\xa0', u'')
             tablelist.append(value)
+ return tablelist
+
+parseHTML(htmlfile)
 
 del tablelist[:9]
 
-#https://www.geeksforgeeks.org/iterate-over-a-list-in-python/
 
 def divide_chunks(l, n):
-
+    # https://www.geeksforgeeks.org/iterate-over-a-list-in-python/
+    # this cuts the list of elements from the HTML table and extracts the rows since table data spans 9 columns
     for i in range(0, len(l), 9):
         yield l[i:i + n]
 
@@ -34,6 +38,8 @@ dividedlist = list(divide_chunks(tablelist, 9))
 def listParsetime(input):
     t = time.strptime(input,'%m/%d/%y')
     return t
+
+#Script Terminal Output
 
 for i in dividedlist:
     print(int(i[1]),i[2],i[3],i[8])
