e9029e1c3712d1903141b2a2c5a5ca0adafbf2e8
RandyN91 <36779831+RandyN91@users.noreply.github.com>
2019-11-14 16:32:30 -0500

    finished alot of functions (#2)

--- main.py
+++ main.py
@@ -1,18 +1,19 @@
 import bs4
-import xlrd
-import datetime
-import time
+import pyad
+from datetime import  datetime as dt
 
+from pyad import pyadutils, adquery
 htmlfile = open('report.html')
+textfile = open('lastlogin.txt',"r")
+
 tablelist = []
 
-excelfile = xlrd.open_workbook("Weekly CM Terminations.xlsx")
-excelSheet = excelfile.sheet_by_index(0)
-
 def parseHTML(input):
- soup = bs4.BeautifulSoup(htmlfile,features="lxml")
+ soup = bs4.BeautifulSoup(input,features="lxml")
  tables = soup.findChildren('table')
+
  my_table = tables[1]
+
  rows = my_table.findChildren(['th', 'tr'])
 
  for row in rows:
@@ -26,7 +27,6 @@ parseHTML(htmlfile)
 
 del tablelist[:9]
 
-
 def divide_chunks(l, n):
     # https://www.geeksforgeeks.org/iterate-over-a-list-in-python/
     # this cuts the list of elements from the HTML table and extracts the rows since table data spans 9 columns
@@ -35,19 +35,48 @@ def divide_chunks(l, n):
 
 dividedlist = list(divide_chunks(tablelist, 9))
 
-def listParsetime(input):
-    t = time.strptime(input,'%m/%d/%y')
-    return t
+#print(dividedlist)
+
+def adsearching(input):
+ try:
+  for i in input:
+
+    q = pyad.adquery.ADQuery()
+    usrid = i[1]
+    nusrid = str("employeeID =" +usrid)
+    q.execute_query(
+        attributes=["mailNickname", "employeeID", "lastLogonTimeStamp"],
+        where_clause= nusrid,
+        base_dn="DC=jetblue,DC=com"
+    )
+
+    for row in q.get_results():
+
+        EmployeeID = (row['employeeID'])
+        LastLogon = (pyadutils.convert_datetime(row['lastLogonTimeStamp']))
+        Name = (row['mailNickname'])
+        LastLogonDate = LastLogon.strftime('%m/%d/%Y')
+
+        #print(type(LastLogonDate))
+        #print(Name, EmployeeID, LastLogonDate)
+
+
+        for i in dividedlist:
+
+         if EmployeeID in i:
+
+          timediff = dt.strptime(LastLogonDate, '%m/%d/%Y') - dt.strptime(i[7],'%m/%d/%Y')
+
+          if timediff.days > 0:
+           print("User " + Name + " Was Terminated on "+str(i[7])+" and last logged in on "+LastLogonDate+" which is "+str(timediff.days)+" days after his termination")
+
+ except ValueError as e :
+     print("Error "+str(e)+" in user "+Name+" Check HTML and AD Manually")
+
 
 #Script Terminal Output
 
-for i in dividedlist:
-    print(int(i[1]),i[2],i[3],i[8])
+print("Output\n")
 
-print("---------")
-
-for i in range(4,excelSheet.nrows):
-
-    print(str(int(excelSheet.cell_value(i,2)))+" "+str(excelSheet.cell_value(i,3))+" "+str(excelSheet.cell_value(i,4))+" "+str(excelSheet.cell_value(i,5))+
-          " "+str(datetime.datetime(*xlrd.xldate_as_tuple((excelSheet.cell_value(i,11)),excelfile.datemode))))
+adsearching(dividedlist)
 
